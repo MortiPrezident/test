ebbcc1fe04c2228c286df64560d30079bc713e6a
danila <danniila2000@mail.ru>
2025-02-21 10:47:58 +0300

    refactor - исправление кода

--- main/main.py
+++ main/main.py
@@ -1,6 +1,7 @@
 from test.main.app import create_app
+
 from waitress import serve
 
 if __name__ == "__main__":
     app = create_app()
-    serve(app, host='0.0.0.0', port=8080)
+    serve(app, host="0.0.0.0", port=8080)
